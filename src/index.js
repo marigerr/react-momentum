e2ef4e90282cede436ab0a3aa7696c5f19bb9f21
marigerr <marigerr.sv@gmail.com>
2017-11-11 13:56:47 +0100

    reg rez background image

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 import 'Src/index.html';
 import 'Src/favicon.ico';
 import App from 'Components/App.jsx';
-/* eslint-disable */
-// import 'Src/CNAME';
-// import 'Src/.surgeignore';
-/* eslint-enable */
 
 ReactDOM.render(<App />, document.getElementById('root'));
