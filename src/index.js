99071a8412e9cd228e573a5ad472ffff57ef9d45
marigerr <marigerr.sv@gmail.com>
2017-12-19 15:44:15 +0100

    import manifest.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'Src/index.html';
 import App from 'Components/App.jsx';
+import 'Src/index.html';
+import 'Src/manifest.json';
 import 'Images/favicons/android-chrome-192x192.png';
 import 'Images/favicons/android-chrome-512x512.png';
 import 'Images/favicons/favicon.ico';
