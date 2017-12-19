a95393faf84f2f13d29cd9bb44b41ef363c570d3
marigerr <marigerr.sv@gmail.com>
2017-12-19 16:04:29 +0100

    troubleshooting web app icons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'Src/index.html';
 import 'Src/manifest.json';
 import 'Images/favicons/android-chrome-192x192.png';
 import 'Images/favicons/android-chrome-512x512.png';
+import 'Images/favicons/apple-touch-icon.png';
 import 'Images/favicons/favicon.ico';
 import 'Images/favicons/favicon-16x16.png';
 import 'Images/favicons/favicon-32x32.png';
